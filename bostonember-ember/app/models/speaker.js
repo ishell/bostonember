e40fb1137db8c72f2a71228d514afd3607472005
ishell <jaysamma@gmail.com>
2015-09-09 11:13:21 +0800

    add presentation

--- bostonember-ember/app/models/speaker.js
+++ bostonember-ember/app/models/speaker.js
@@ -1,5 +1,6 @@
-import Model, { attr } from 'ember-data/model';
+import DS from 'ember-data';
 
-export default Model.extend({
-  name: attr('string')
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  presentations: DS.hasMany('presentation')
 });
--- bostonember-rails/db/schema.rb
+++ bostonember-rails/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909021845) do
+ActiveRecord::Schema.define(version: 20150909030608) do
+
+  create_table "presentations", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.integer  "speaker_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "speakers", force: :cascade do |t|
     t.string   "name",       limit: 255
